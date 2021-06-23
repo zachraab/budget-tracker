c2e52c408a14e99b84a59b0eed99df208edb2df6
Zach Raab <zpraab@hotmail.com>
2021-06-23 01:13:22 -0600

    added test data to objectStore

--- public/db.js
+++ public/db.js
@@ -8,23 +8,33 @@ let db;
 //  create a database
 const request = indexedDB.open("budgetTracker", 1);
 
-request.onsuccess = () => {
-    console.log(request.result)
-}
-
 //  create an object store - so we can store data
 request.onupgradeneeded = ({ target }) => {
     let budgetStore;
     db = target.result;
     // if there isn't an objectStore already, create one with autoIncrementing unique id's
     if (db.objectStoreNames.length === 0) {
-       budgetStore = db.createObjectStore('BudgetTracker', { autoIncrement: true });
+       budgetStore = db.createObjectStore('BudgetTracker', { 
+        autoIncrement: true,
+        keyPath: "ID" 
+      });
       }
       //  create indexes - so we can query the data
-    budgetStore.createIndex("test", "testKeyPath")
+    budgetStore.createIndex("testIndex", "testKeyPath")
 };
 
-
+request.onsuccess = () => {
+db = request.result
+//give access to db with readwrite privileges 
+const transaction = db.transaction(["BudgetTracker"], "readwrite");
+//grab objectStore
+const budgetStore = transaction.objectStore("BudgetTracker");
+//grab the index
+const testIndex = budgetStore.index("testIndex")
 
 //  add data to the database
+budgetStore.add({ ID: "1", testKeyPath: "Hello World!"})
+budgetStore.add({ ID: "2", testKeyPath: "test data"})
+}
+
 //  update data with cursors
