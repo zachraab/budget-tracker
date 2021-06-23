ccb43ac861a9faf643203a75e245a4d14dc280cd
Zach Raab <zpraab@hotmail.com>
2021-06-23 00:37:25 -0600

    created object store to store data inside

--- public/db.js
+++ public/db.js
@@ -8,6 +8,10 @@ request.onsuccess = () => {
     console.log(request.result)
 }
 //  create an object store - so we can store data
+request.onupgradeneeded = ({ target }) => {
+    const db = target.result;
+    const objectStore = db.createObjectStore("budgetTracker");
+};
 //  create indexes - so we can query the data
 //  add data to the database
 //  update data with cursors
