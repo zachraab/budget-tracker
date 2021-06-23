55c9f821837e1e0c3771e1bb08d6f84f75632a43
Zach Raab <zpraab@hotmail.com>
2021-06-23 00:52:30 -0600

    created test index to query data

--- public/db.js
+++ public/db.js
@@ -1,17 +1,30 @@
 //indexedDB pseudo code
 //
 
+// GLOBAL VARIABLES
+let db;
+
+
 //  create a database
 const request = indexedDB.open("budgetTracker", 1);
 
 request.onsuccess = () => {
     console.log(request.result)
 }
+
 //  create an object store - so we can store data
 request.onupgradeneeded = ({ target }) => {
-    const db = target.result;
-    const objectStore = db.createObjectStore("budgetTracker");
+    let budgetStore;
+    db = target.result;
+    // if there isn't an objectStore already, create one with autoIncrementing unique id's
+    if (db.objectStoreNames.length === 0) {
+       budgetStore = db.createObjectStore('BudgetTracker', { autoIncrement: true });
+      }
+      //  create indexes - so we can query the data
+    budgetStore.createIndex("test", "testKeyPath")
 };
-//  create indexes - so we can query the data
+
+
+
 //  add data to the database
 //  update data with cursors
